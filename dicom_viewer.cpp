b21818eab5a5de7d8b5b50ec0bb9afc17c55b153
Gabriel Dubois <gabriel.dubois@etu.u-bordeaux.fr>
2021-09-13 14:41:25 +0200

    Init code

---
+++ dicom_viewer.cpp
@@ -0,0 +1,38 @@
+#include "dicom_viewer.h"
+
+#include <iostream>
+
+#include <QFileDialog>
+#include <QMenuBar>
+#include <QMessageBox>
+
+DicomViewer::DicomViewer(QWidget *parent) : QMainWindow(parent) {
+  // Setting menu
+  QMenu *file_menu = menuBar()->addMenu("&File");
+  QAction *open_action = file_menu->addAction("&Open");
+  open_action->setShortcut(QKeySequence::Open);
+  QObject::connect(open_action, SIGNAL(triggered()), this, SLOT(openDicom()));
+  QAction *help_action = file_menu->addAction("&Help");
+  help_action->setShortcut(QKeySequence::HelpContents);
+  QObject::connect(help_action, SIGNAL(triggered()), this, SLOT(showStats()));
+}
+
+DicomViewer::~DicomViewer() {}
+
+void DicomViewer::openDicom() {}
+
+void DicomViewer::showStats() {
+  std::ostringstream msg_oss;
+  msg_oss << "Patient: TODO" << std::endl;
+  msg_oss << "Instance number: TODO" << std::endl;
+  msg_oss << "Acquisition number: TODO" << std::endl;
+  msg_oss << "Original transfer syntax: TODO" << std::endl;
+  msg_oss << "Nb frames: TODO" << std::endl;
+  msg_oss << "Size: W*H*D" << std::endl;
+  msg_oss << "Allowed values: [TODO,TODO]" << std::endl;
+  msg_oss << "Used values: [TODO,TODO]" << std::endl;
+  msg_oss << "Window: [TODO,TODO]" << std::endl;
+  msg_oss << "Slope: TODO" << std::endl;
+  msg_oss << "Intercept: TODO" << std::endl;
+  QMessageBox::information(this, "DCM file properties", msg_oss.str().c_str());
+}
---
+++ dicom_viewer.h
@@ -0,0 +1,24 @@
+#ifndef DICOM_VIEWER_H
+#define DICOM_VIEWER_H
+
+#include <QMainWindow>
+#include <QVBoxLayout>
+
+#include "dcmtk/dcmdata/dcrledrg.h"
+#include "dcmtk/dcmdata/dctk.h"
+#include "dcmtk/dcmimgle/dcmimage.h"
+#include "dcmtk/dcmjpeg/djdecode.h"
+
+class DicomViewer : public QMainWindow {
+  Q_OBJECT
+
+public:
+  DicomViewer(QWidget *parent = 0);
+  ~DicomViewer();
+
+public slots:
+  void openDicom();
+  void showStats();
+};
+
+#endif // DICOM_VIEWER_H
